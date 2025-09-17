a3d60e55e3d78a969b2c6da193e73dcc84902623
Odeguy <lereoatschool@gmail.com>
2025-09-17 17:16:23 -0400

    select screen split by series

--- selection_screen_script.gd
+++ selection_screen_script.gd
@@ -5,9 +5,10 @@ class_name Selection_Screen
 var balls
 func _ready() -> void:
 	balls = JSON.parse_string(FileAccess.get_file_as_string("res://balls.json"))
-	for name in balls:
+	for name in balls["first_series"]:
 		$ball_grid.add_item(name)
-	
+	for name in balls["burst_series"]:
+		$burst_ball_grid.add_item(name)
 var selection
 var previous
 func get_selections() -> Array:
@@ -19,18 +20,36 @@ func get_selections() -> Array:
 			await get_tree().process_frame
 		if i < 4:
 			if selection == "Done": break
-			var ball_scene = load(balls[selection])
+			var ball_scene = load(selection)
 			var ball: Ball = ball_scene.instantiate()
 			selections.push_back(ball)
 			previous = ball
 			ball.team += str(randi())
 	return selections
+
+@onready var shadow_max = $Label2.get_theme_constant("shadow_offset_x")
+@onready var dshadow = -1
+@onready var count = 0
+func _process(delta: float) -> void:
+	count += 1
+	if count % 15 != 0: return
+	var new_offset = $Label2.get_theme_constant("shadow_offset_x") + dshadow
+	$Label2.add_theme_constant_override("shadow_offset_x", new_offset)
+	$Label2.add_theme_constant_override("shadow_offset_y", new_offset)
+	$Label3.add_theme_constant_override("shadow_offset_x", new_offset)
+	$Label3.add_theme_constant_override("shadow_offset_y", new_offset)
+	if $Label2.get_theme_constant("shadow_offset_x") == 2 || $Label2.get_theme_constant("shadow_offset_x") == shadow_max: dshadow *= -1
 		
 
 func _on_ball_grid_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
 	$AudioStreamPlayer2D.play()
 	if mouse_button_index == 1:
-		selection = $ball_grid.get_item_text(index)
+		selection = balls["first_series"][$ball_grid.get_item_text(index)]
+
+func _on_burst_ball_grid_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	$AudioStreamPlayer2D.play()
+	if mouse_button_index == 1:
+		selection = balls["burst_series"][$burst_ball_grid.get_item_text(index)]
 
 
 func _on_button_pressed() -> void:
